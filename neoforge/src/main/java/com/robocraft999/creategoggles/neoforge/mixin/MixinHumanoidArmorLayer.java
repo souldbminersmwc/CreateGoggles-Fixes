c9f1bc612f9ed3467622f1158e436a1d13d2e151
Souldbminer <162390887+souldbminersmwc@users.noreply.github.com>
2025-07-15 13:03:07 -0400

    Update MixinHumanoidArmorLayer.java

--- neoforge/src/main/java/com/robocraft999/creategoggles/neoforge/mixin/MixinHumanoidArmorLayer.java
+++ neoforge/src/main/java/com/robocraft999/creategoggles/neoforge/mixin/MixinHumanoidArmorLayer.java
@@ -1,28 +1,34 @@
 package com.robocraft999.creategoggles.neoforge.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.robocraft999.creategoggles.registry.CGTrimPatterns;
-import net.minecraft.client.model.Model;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
-import net.minecraft.core.Holder;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.armortrim.ArmorTrim;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.trim.ArmorTrim;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HumanoidArmorLayer.class)
-public abstract class MixinHumanoidArmorLayer {
+@Mixin(ArmorFeatureRenderer.class)
+public abstract class MixinArmorFeatureRenderer<T, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
 
     @Inject(
-            method = "renderTrim*",
-            at = @At("HEAD"),
-            cancellable = true
+        method = "renderTrim(Lnet/minecraft/item/ArmorMaterial;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/item/trim/ArmorTrim;Lnet/minecraft/client/render/entity/model/BipedEntityModel;Z)V",
+        at = @At("HEAD"),
+        cancellable = true
     )
-    public void onRenderArmorTrim(Holder<ArmorMaterial> armorMaterial, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, ArmorTrim armorTrim, Model humanoidModel, boolean bl, CallbackInfo ci){
-        if (armorTrim.material().is(CGTrimPatterns.GOGGLE_MATERIAL)){
+    private void onRenderTrim(ArmorMaterial material,
+                              MatrixStack matrices,
+                              VertexConsumerProvider vcp,
+                              int light,
+                              ArmorTrim trim,
+                              BipedEntityModel<T> model,
+                              boolean slim,
+                              CallbackInfo ci) {
+        if (trim != null && trim.material().is(CGTrimPatterns.GOGGLE_MATERIAL)) {
             ci.cancel();
         }
     }
